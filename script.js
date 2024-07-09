e5c859c14bffc03a155785262bf62bfdb6e0e764
Gabriel Pontes <gabriel.hgarsk@gmail.com>
2024-07-09 17:50:20 -0300

    Avançando rápido em arrow function, objects, manipulação do DOM e condicionais em js

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
+// objeto em js - {}
+const atividade = {
+    nome: 'Almoço',
+    data: new Date('2024-07-09 10:00'),
+    finalizada: false
+}
+
+// arrow function
+const criarItemAtividade = (atividade) => {
+    let input = '<input type="checkbox"'
+
+    if(atividade.finalizada) {
+        input = input + 'checked'
+    }
+
+    input = input + '>'
+
+    return `
+    <div>
+        ${input}
+        <span>${atividade.nome}</span>
+        <time>${atividade.data}</time>
+    </div>
+    `
+}
+
 const section = document.querySelector('section')
-section.innerHTML = 'Alô!'
+section.innerHTML = criarItemAtividade(atividade)
+
